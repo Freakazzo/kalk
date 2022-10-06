b089967128ea279df635d905ef2a4578e1962ec5
Freakazzo <105446360+Freakazzo@users.noreply.github.com>
2022-10-06 13:26:22 +0400

    Up

--- src/kalk.java
+++ src/kalk.java
@@ -2,37 +2,45 @@ import java.util.Scanner;
 
 public class kalk {
     public static void main(String[] args) {
-        int operand1;
-        int operand2;
-        int otvet;
+        double ch1;
+        double ch2;
+        double otvet;
         char operation;
+        char calculatorOperator = 'c';
+        double result = 0;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Vvedite chislo");
-        operand1 = scanner.nextInt();
-        operand2 = scanner.nextInt();
-        System.out.println("Vvedite (+,-,*,/)");
+        System.out.println("Введите первое число");
+        ch1 = scanner.nextInt();
+
+        System.out.println("Введите операцию (+,-,*,/)");
         operation = scanner.next().charAt(0);
+
+        System.out.println("Введите второе число");
+        ch2 = scanner.nextInt();
+
+
+
 
         switch (operation) {
             case '+':
-                otvet = (operand1 + operand2);
+                otvet = (ch1 + ch2);
                 break;
             case '-':
-                otvet = (operand1 - operand2);
+                otvet = (ch1 - ch2);
                 break;
             case '*':
-                otvet = (operand1 * operand2);
+                otvet = (ch1 * ch2);
                 break;
             case '/':
-                otvet = (operand1 / operand2);
+                otvet = (ch1 / ch2);
                 break;
             default:
                 System.out.println("Error!");
-                return;
+                return ;
         }
-        System.out.println("Rezult");
-        System.out.println(operand1 + " " + operation + " " + operand2 + " = " + otvet);
+        System.out.println("Ответ");
+        System.out.println(ch1 + " " + operation + " " + ch2 + " = " + otvet);
     }
 }
 
