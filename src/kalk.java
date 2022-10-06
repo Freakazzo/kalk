fb4d19c6427a0df1f607d7542042708d5cc48041
Freakazzo <105446360+Freakazzo@users.noreply.github.com>
2022-10-06 14:02:44 +0400

    Finish!

--- src/kalk.java
+++ src/kalk.java
@@ -2,45 +2,48 @@ import java.util.Scanner;
 
 public class kalk {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         double ch1;
         double ch2;
-        double otvet;
-        char operation;
+        char operation = 0;
+        double otvet = 0;
         char calculatorOperator = 'c';
-        double result = 0;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число");
-        ch1 = scanner.nextInt();
-
-        System.out.println("Введите операцию (+,-,*,/)");
-        operation = scanner.next().charAt(0);
-
-        System.out.println("Введите второе число");
-        ch2 = scanner.nextInt();
 
 
+        do {
+            if (calculatorOperator == 'c') {
+                System.out.println("Введите первое число");
+                ch1 = scanner.nextDouble();
+                System.out.println("Введите операцию (+,-,*,/)");
+                operation = scanner.next().charAt(0);
+            } else {
+                ch1 = otvet;
 
+            }
+            System.out.println("Введите второе число");
+            ch2 = scanner.nextDouble();
+            otvet = kalkulaytet(ch1, ch2, operation);
 
-        switch (operation) {
-            case '+':
-                otvet = (ch1 + ch2);
-                break;
-            case '-':
-                otvet = (ch1 - ch2);
-                break;
-            case '*':
-                otvet = (ch1 * ch2);
-                break;
-            case '/':
-                otvet = (ch1 / ch2);
-                break;
-            default:
+            System.out.println(ch1 + " " + operation + " " + ch2 + " = " + otvet);
+
+            calculatorOperator = scanner.next().charAt(0);
+
+        } while (calculatorOperator != 's');
+
+    }
+
+    public static double kalkulaytet(double ch1, double ch2, char operation) {
+
+        return switch (operation) {
+            case '+' -> ch1 + ch2;
+            case '-' -> ch1 - ch2;
+            case '*' -> ch1 * ch2;
+            case '/' -> ch1 / ch2;
+            default -> {
                 System.out.println("Error!");
-                return ;
-        }
-        System.out.println("Ответ");
-        System.out.println(ch1 + " " + operation + " " + ch2 + " = " + otvet);
+                yield 0;
+            }
+        };
     }
 }
 
